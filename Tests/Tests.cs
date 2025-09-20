ce3ed569a4cf3a6177712db92760202f55e11e2e
Dallin Osorio <dallin.osorio@outlook.com>
2025-09-20 12:22:35 -0600

    Se agregan los test

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -22,40 +22,50 @@ namespace Tests
         public void PrestarLibro_LibroDisponible_PrestaLibroCorrectamente()
         {
             // Act
+            _biblioteca.PrestarLibro("1984");
 
             // Assert
+           Assert.That(_libro1.EstaPrestado, Is.True);
         }
 
         [Test]
         public void PrestarLibro_LibroNoDisponible_LanzaExcepcion()
         {
             // Act
-
+            var resultado = Assert.Throws<InvalidOperationException>(() => _biblioteca.PrestarLibro("test"));
             // Assert
+            Assert.That(resultado.Message.ToString(), Is.EqualTo("El libro no se encuentra en la biblioteca."));
         }
 
         [Test]
         public void DevolverLibro_LibroPrestado_DevolveLibroCorrectamente()
         {
             // Act
-
+            _biblioteca.PrestarLibro("1984");
+            _biblioteca.DevolverLibro("1984");
+            
             // Assert
+            Assert.That(_libro1.EstaPrestado, Is.False);
         }
 
         [Test]
         public void DevolverLibro_LibroNoPrestado_LanzaExcepcion()
         {
             // Act
-
+            var resultado = Assert.Throws<InvalidOperationException>(() => _biblioteca.DevolverLibro("1984"));
             // Assert
+            Assert.That(resultado.Message.ToString(), Is.EqualTo("El libro no está prestado."));
         }
 
         [Test]
         public void ObtenerLibros_RetornaListaDeLibros()
         {
             // Act
-
+            var libros = _biblioteca.ObtenerLibros();
+            
             // Assert
+            Assert.That(libros.Count, Is.EqualTo(2));
+
         }
     }
 }
